63be3c9e62c307aba883d4f2a36ae96bca3867dc
Haojin Tang <tanghaojin@outlook.com>
2021-08-21 20:18:02 +0800

    feat: add sim IO device for DMA tests

---
+++ playground/sim/src/sim/peripheral/storage/storage.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <svdpi.h>
+#include <debug.hpp>
+
+#define PMEM_SIZE (128 * 1024 * 1024)
+
+#define PAGE_SIZE 4096
+#define PAGE_MASK (PAGE_SIZE - 1)
+#define PG_ALIGN __attribute((aligned(PAGE_SIZE)))
+
+static inline bool in_pmem(uint64_t addr) {
+  return (addr < PMEM_SIZE);
+}
+
+static uint8_t pmem[PMEM_SIZE] PG_ALIGN = {};
+
+extern "C" void storage_read(uint64_t addr, uint64_t *data) {
+  if (!data) return;
+  *data = in_pmem(addr) ? *(uint64_t *)(pmem + addr) : 0xBB;
+}
+
+extern "C" void storage_write(uint64_t addr, uint64_t data, uint8_t mask) {
+  uint8_t *p = &pmem[addr];
+  if (in_pmem(addr))
+    for (int i = 0; i < 8; i++) {
+      if (mask & 1) *p = data;
+      p++;
+      data >>= 8;
+      mask >>= 1;
+    }
+}
+
+extern "C" void storage_init(char *img) {
+  FILE *fp = fopen(img, "rb");
+  Assert(fp, "Can not open '%s'", img);
+
+  fseek(fp, 0, SEEK_END);
+  uint64_t size = ftell(fp);
+
+  fseek(fp, 0, SEEK_SET);
+  assert(fread(pmem, size, 1, fp) == 1);
+
+  fclose(fp);
+}
