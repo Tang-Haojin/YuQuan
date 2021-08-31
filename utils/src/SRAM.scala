1b77a2f48987bdd79833dd1dde784c99b5b07401
Haojin Tang <tanghaojin@outlook.com>
2021-08-31 16:09:27 +0800

    fix: typo in `SRAM` inline blackbox

--- utils/src/SRAM.scala
+++ utils/src/SRAM.scala
@@ -4,34 +4,34 @@ import chisel3._
 import chisel3.util._
 import chipsalliance.rocketchip.config._
 
-class SRAM(bits: Int = 128, wordDepth: Int = 64, addrWidth: Int = 6)(implicit val p: Parameters) extends BlackBox with HasBlackBoxInline with PrefixParams {
+class SRAM(bits: Int = 128, wordDepth: Int = 64)(implicit val p: Parameters) extends BlackBox with HasBlackBoxInline with PrefixParams {
   val io = IO(new Bundle {
     val Q   = Output(UInt(bits.W))
     val CLK = Input (Clock())
     val CEN = Input (Bool())
     val WEN = Input (Bool())
-    val A   = Input (UInt(addrWidth.W))
+    val A   = Input (UInt(log2Ceil(wordDepth).W))
     val D   = Input (UInt(bits.W))
   })
   override val desiredName = modulePrefix + this.getClass().getSimpleName()
   this.setInline(desiredName + ".v", s"""
     |module ${desiredName} (
-    |  output reg [${bits - 1}:0]      Q,
-    |  input                           CLK,
-    |  input                           CEN,
-    |  input                           WEN,
-    |  input      [${addrWidth - 1}:0] A,
-    |  input      [${bits - 1}:0]      D
+    |  output reg [${bits - 1}:0]                Q,
+    |  input                                     CLK,
+    |  input                                     CEN,
+    |  input                                     WEN,
+    |  input      [${log2Ceil(wordDepth) - 1}:0] A,
+    |  input      [${bits - 1}:0]                D
     |);
     |
     |  reg [${bits - 1}:0] ram [0:${wordDepth - 1}];
     |  always @(posedge CLK) begin
-    |    if (!CEN && !CEN) begin
+    |    if (!CEN && !WEN) begin
     |      ram[A] <= D;
     |    end
     |    Q <= !CEN && WEN ? ram[A] : {${bits / 32}{32'hdead_feed}};
     |  end
     |
     |endmodule
-  """)
+  """.stripMargin)
 }
