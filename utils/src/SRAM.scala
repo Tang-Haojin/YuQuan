299fec56cda63fc00e3ce755909b4647111a37ed
Haojin Tang <tanghaojin@outlook.com>
2021-08-31 16:11:36 +0800

    feat: implement SyncReadMem-like SinglePortRam

--- utils/src/SRAM.scala
+++ utils/src/SRAM.scala
@@ -35,3 +35,48 @@ class SRAM(bits: Int = 128, wordDepth: Int = 64)(implicit val p: Parameters) ext
     |endmodule
   """.stripMargin)
 }
+
+object SRAM {
+  def apply(bits: Int = 128, wordDepth: Int = 64)(implicit p: Parameters): SRAM = Module(new SRAM(bits, wordDepth))
+}
+
+class SramWrapper(clock: Clock, bits: Int = 128, wordDepth: Int = 64)(implicit val p: Parameters) extends PrefixParams {
+  private val sram = SRAM(bits, wordDepth)
+  private val rAddr = WireDefault(0.U(log2Ceil(wordDepth).W))
+  private val wAddr = WireDefault(0.U(log2Ceil(wordDepth).W))
+
+  sram.io.CLK := clock
+  sram.io.CEN := 0.B
+  sram.io.WEN := 1.B
+  sram.io.A   := rAddr
+  sram.io.D   := 0.U
+
+  def read(x: UInt, en: Bool = 1.B): UInt = { rAddr := x; sram.io.Q }
+
+  def write(idx: UInt, data: UInt, wen: Bool): Unit = {
+    wAddr       := idx
+    sram.io.WEN := !wen
+    sram.io.D   := data
+    when(!sram.io.WEN) { sram.io.A := wAddr }
+  }
+}
+
+object SramWrapper {
+  def apply(clock: Clock, bits: Int = 128, wordDepth: Int = 64)(implicit p: Parameters): SramWrapper = new SramWrapper(clock, bits, wordDepth)
+}
+
+class SinglePortRam(clock: Clock, bits: Int = 128, wordDepth: Int = 64, associativity: Int = 4)(implicit val p: Parameters) extends PrefixParams {
+  private val SRAMs = Seq.fill(associativity)(SramWrapper(clock, bits, wordDepth))
+
+  def read(x: UInt, en: Bool = 1.B): Vec[UInt] = {
+    VecInit(Seq.tabulate(associativity)(y => { SRAMs(y).read(x, en) }))
+  }
+
+  def write(idx: UInt, data: Vec[UInt], mask: Seq[Bool]): Unit = {
+    for (i <- SRAMs.indices) { SRAMs(i).write(idx, data(i), mask(i)) }
+  }
+}
+
+object SinglePortRam {
+  def apply(clock: Clock, bits: Int = 128, wordDepth: Int = 64, associativity: Int = 4)(implicit p: Parameters): SinglePortRam = new SinglePortRam(clock, bits, wordDepth, associativity)
+}
