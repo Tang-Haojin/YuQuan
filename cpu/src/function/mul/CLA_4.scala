e9522ee4dff925c0ff38f01ebae8b8f174bc5543
Haojin Tang <tanghaojin@outlook.com>
2021-09-02 21:01:12 +0800

    fix(CLA): avoid using uninitialized `C` in itself

--- cpu/src/function/mul/CLA_4.scala
+++ cpu/src/function/mul/CLA_4.scala
@@ -1,4 +1,4 @@
-package function.mul
+package cpu.function.mul
 
 import chisel3._
 import chisel3.util._
@@ -17,9 +17,9 @@ class CLA_4(implicit p: Parameters) extends YQRawModule {
   private val P = Seq.tabulate(4)(x => io.op1(x) ^ io.op2(x))
   private val C:  Seq[Bool] = Seq(
     io.cin,
-    G(0) ^ (C(0) & P(0)),
-    G(1) ^ ((G(0) ^ (C(0) & P(0))) & P(1)),
-    G(2) ^ ((G(1) ^ ((G(0) ^ (C(0) & P(0))) & P(1))) & P(2))
+    G(0) ^ (io.cin & P(0)),
+    G(1) ^ ((G(0) ^ (io.cin & P(0))) & P(1)),
+    G(2) ^ ((G(1) ^ ((G(0) ^ (io.cin & P(0))) & P(1))) & P(2))
   )
   io.sum  := (P(3) ## P(2) ## P(1) ## P(0)) ^ (C(3) ## C(2) ## C(1) ## C(0))
   io.cout := G(3) ^ ((G(2) ^ ((G(1) ^ ((G(0) ^ (C(0) & P(0))) & P(1))) & P(2))) & P(3))
