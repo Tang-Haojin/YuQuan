9871d6ef3bba205383eaa6bd7bbb28a7b8511197
Haojin Tang <tanghaojin@outlook.com>
2021-09-14 21:06:50 +0800

    feat: allow writing xstatus's `MXR` and `SUM` bits

--- cpu/src/privileged/complexCSRs.scala
+++ cpu/src/privileged/complexCSRs.scala
@@ -35,10 +35,10 @@ class MstatusBundle(implicit val p: Parameters) extends Bundle with CPUParams {
 
   def := (that: => Data): Unit = {
     val mstatus = WireDefault(new MstatusBundle, that.asTypeOf(new MstatusBundle))
+    MXR := mstatus.MXR; SUM := mstatus.SUM
     MPP := Mux(mstatus.MPP =/= 2.U, mstatus.MPP, MPP)
     SPP := mstatus.SPP; MPIE := mstatus.MPIE; SPIE := mstatus.SPIE; UPIE := mstatus.UPIE
-    MIE := mstatus.MIE; SIE := mstatus.SIE; UIE := mstatus.UIE
-    TSR := mstatus.TSR
+    MIE := mstatus.MIE; SIE := mstatus.SIE; UIE := mstatus.UIE; TSR := mstatus.TSR
   }
 }
 
